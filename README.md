19dcfcdd3efd73f2941f7ff42ac16f55f23a7fdb
mabeijing <58149278@qq.com>
2023-04-12 15:19:16 +0800

    新增uwsgi线上部署

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # flask-vue-robots
 flask
+
+1. 环境配置-uwsgi线上部署
---
+++ uwsgi.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+uwsgi --version
+
+#停止uWSGI
+#uwsgi --stop /var/run/uwsgi.pid
+
+#使用uwsgi.ini配置文件启动wsgi应用程序
+uwsgi --ini uwsgi.ini
+
+##重启uWSGI服务器
+#uwsgi --reload /var/run/uwsgi.pid
+
+
+##查看所有uWSGI进程
+#ps aux | grep uwsgi
+#ps -eLf | grep uwsgi
+
+##停止所有uWSGI进程
+#sudo pkill -f uwsgi -9
+
+

ade14c40ff7e8cc2b8da9f6679e38f60bc4848ba
mabeijing <58149278@qq.com>
2023-05-15 17:17:02 +0800

    没啥上传

--- flask_vue_robots/__init__.py
+++ flask_vue_robots/__init__.py
@@ -2,21 +2,35 @@ from flask import Flask, render_template
 from l10n import bp_l10n
 from flask_vue_robots.error_handles import base_exception_handle
 from werkzeug.exceptions import HTTPException
+from tasks import celery_init_app
+
+import importlib
 
 
 # http://127.0.0.1:5000/images/1.txt
 app = Flask(__name__, static_folder='../static', static_url_path='/images', template_folder='templates')
-# app.config['SECRET_KEY'] = 'secret!'
 
+cfg = importlib.import_module(f"{__package__}.settings")
+app.config.from_object(cfg.DevelopConfig)
+
+celery_init_app(app)
 app.register_blueprint(bp_l10n)
 
 
 @app.get("/")
 def index():
+    app.logger.info("dcsdcsc")
     return "welcome to Hello World!"
+
+
+@app.get("/run")
+def run():
+    pass
+
 
 @app.errorhandler(Exception)
 def error_handle(e):
     return render_template("error404.html")
+
 
 app.register_error_handler(HTTPException, base_exception_handle)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flask_vue_robots import app
 
+celery_app = app.extensions["celery"]
+
 if __name__ == '__main__':
     app.run()

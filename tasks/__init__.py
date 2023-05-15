8636099f25f2a2552a4c754ff82f400a2b5a7f77
mabeijing <58149278@qq.com>
2023-05-15 17:17:48 +0800

    celery task

---
+++ tasks/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from celery import Celery, Task
+
+
+def celery_init_app(app: Flask) -> Celery:
+    class TaskWrapper(Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=TaskWrapper)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+
+    return celery_app

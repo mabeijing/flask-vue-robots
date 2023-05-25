134d9bef4d657f125f2f561c24f0c4b0f4f6e491
mabeijing <58149278@qq.com>
2023-05-25 17:33:36 +0800

    增加celery的异步任务和定时任务

--- tasks/asynchronous.py
+++ tasks/asynchronous.py
@@ -6,4 +6,7 @@ from flask_vue_robots.extension.ext_celery import celery
 @celery.task(bind=True)
 def send_report(self, x, y):
     time.sleep(8)
+    print(f"Task => {id(self)}")  # 多进程下，这里获取的是app的fork对象
+    print(f"Celery => {self.app}")  # 多进程下，这里获取的是app的fork对象
+    print(f"Flask => {id(self.app.flask_app)}")  # 多进程下，这里获取的是app的fork对象
     return x + y
--- tasks/scheduler.py
+++ tasks/scheduler.py
@@ -3,9 +3,8 @@ import time
 from flask_vue_robots.extension.ext_celery import celery
 
 
-
 @celery.task
 def send_ok():
     time.sleep(3)
-
+    celery.flask_app.logger.warning(f"flask_app=>{id(celery.flask_app)}")
     return "OK"
